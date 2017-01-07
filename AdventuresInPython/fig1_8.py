deb88f2c0b6dfd44c687b1c7c2e3b57cce759771
Cigwyt <chrisharper23@hotmail.com>
2017-01-07 01:04:14 -0700

    Delete fig1_8.py

--- AdventuresInPython/fig1_8.py
+++
@@ -1,7 +0,0 @@
-personName = raw_input("Enter a name: ")
-anObject = raw_input("Enter an object: ")
-place = raw_input("Enter a place: ")
-
-story = personName + " was walking through " + place + ". " + place + " was not usually very interesting. \n" +  personName + " spotted a small " + anObject + ". Suddently  the " + anObject + " jumped up and ran away. \n " + personName + " decided not to go to " + place + " agian. "
-
-print story
